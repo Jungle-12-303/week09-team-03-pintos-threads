73de4ed918e7db1e9859bf5780a0d2dc07b496a5
Cho1022 <cbs001022@gmail.com>
2026-04-30 15:33:12 +0900

    static compare 수정

--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -390,7 +390,7 @@ thread_yield (void) {
 	intr_set_level (old_level);
 }
 
-bool compare (const struct list_elem *a, const struct list_elem *b, void *aux) 
+static bool compare (const struct list_elem *a, const struct list_elem *b, void *aux) 
 {
 	struct thread *thread_a = list_entry(a, struct thread, elem);
 	struct thread *thread_b = list_entry(b, struct thread, elem);
