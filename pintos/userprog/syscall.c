1f168c9f07bcb1ca7fc3ea18fd778770d4083800
Cho1022 <cbs001022@gmail.com>
2026-05-04 10:48:12 +0900

    fix: write/exit구현

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -8,6 +8,11 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 
+//NICK
+#include <kernel/stdio.h>
+#include "threads/vaddr.h"
+//NICK
+
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
 
@@ -39,8 +44,54 @@ syscall_init (void) {
 
 /* The main system call interface */
 void
-syscall_handler (struct intr_frame *f UNUSED) {
+syscall_handler (struct intr_frame *f ) {
+	
+	//NICK - 자식 프로그램이 종료하려면 -> exit() system call를 kenel에 보낸다.
+	// printf ("[trace] syscall no=%d, arg1=%d, arg3=%d\n",
+	// 		(int) f->R.rax,
+	// 		(int) f->R.rdi,
+	// 		(int) f->R.rdx);
+
+	//f는 syscall이 발생한 순간의 CPU 레지스터 상태를 담은 intr_frame 구조체 포인터다.
+	//f->R.rax는 그 안에 저장된 rax 레지스터 값이다.
+	
+	switch(f -> R.rax){
+		case SYS_WRITE: 
+		{
+			int fd = (int)f->R.rdi; //sys_write 번호== 10 이고 rdi에서 읽어와야함
+			const char *buffer = (const char*)f->R.rsi;
+			size_t size = (size_t)f->R.rdx;
+
+			// == 와 = 의 차이 
+			if(buffer == NULL || !is_user_vaddr(buffer))
+			{
+				//buffer 주소가 유저 영역 주소가 아니라면? -> 여기해석은 write() 호출 결과로 -1를 반환한다. 
+				f->R.rax = -1;
+				return;
+			}
+			if(fd == 1 )
+			{
+				putbuf(buffer, size);
+				f->R.rax = size; 
+			}else
+			{
+				f->R.rax = -1;
+			}
+			break;
+		}
+		case SYS_EXIT:
+		{
+			//int status = (int)f->R.rdi;
+			//printf("%s: exit(%d)\n", thread_current() -> name, status);
+			thread_exit();
+			break;
+		}
+
+		default:
+			
 	// TODO: Your implementation goes here.
-	printf ("system call!\n");
-	thread_exit ();
+	//printf ("system call!\n");
+		thread_exit ();
+	}
+	//NICk
 }
