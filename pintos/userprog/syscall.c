cdf3d55d0e61d8f7a622ff90ac5ce2c298635ab7
Cho1022 <cbs001022@gmail.com>
2026-05-06 15:15:01 +0900

    fix: syscall-create

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -8,6 +8,13 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 
+/* NICK */
+#include "filesys/filesys.h"
+#include "threads/synch.h"
+#include "threads/vaddr.h"
+#include "threads/mmu.h"
+/* NICK */
+
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
 
@@ -24,6 +31,11 @@ void syscall_handler (struct intr_frame *);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+	/* NICK */
+
+static struct lock filesys_lock;
+	/* NICK */
+
 void
 syscall_init (void) {
 	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
@@ -35,6 +47,54 @@ syscall_init (void) {
 	 * mode stack. Therefore, we masked the FLAG_FL. */
 	write_msr(MSR_SYSCALL_MASK,
 			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+	
+	/* NICK */
+	lock_init(&filesys_lock);
+	/* NICK */
+
+}
+
+static void
+sys_exit (int status) {
+	struct thread *curr = thread_current ();
+	curr->exit_code = status;
+	thread_exit ();
+}
+
+static void 
+validate_user_addr (const void *uaddr) {
+	struct thread *curr = thread_current ();
+
+	if (uaddr == NULL ||
+		!is_user_vaddr (uaddr) ||
+		curr->pml4 == NULL ||
+		pml4_get_page (curr->pml4, uaddr) == NULL) {
+		sys_exit (-1);
+	}
+}
+
+static void //1.유저가 넘긴 문자열이 유효한지 검증하는 함수
+validate_user_string(const char *str)
+{
+	while(true)
+	{
+		validate_user_addr(str);
+
+		if(*str ==  '\0') return;
+		str++; 
+	}
+} 
+
+
+static void //2. 버퍼가 유저 영역에 있는지 검증하는 함수
+validate_user_buffer(const void *buffer, size_t size)
+{
+	const uint8_t *buf = buffer; 
+
+	for(size_t i = 0; i < size; i++)
+	{
+		validate_user_addr(buf + i); //
+	}
 }
 
 /* The main system call interface */
@@ -44,12 +104,74 @@ syscall_handler (struct intr_frame *f UNUSED) {
 	// printf ("system call!\n");
 
 	/* SONNY'S CODE */
-	switch ((int)(f->R.rax))
+	switch ((int)(f->R.rax)) //이곳에 write, exit, create , open ..
 	{
-	
-	case SYS_WRITE: /* write -SONNY- */
-		putbuf ((char*)(f->R.rsi), (size_t)(f->R.rdx));
-		break;
+	/* NICK */
+	case SYS_WRITE:
+	{
+		int fd = (int) f -> R.rdi; 
+		const char *buffer = (const char *)f -> R.rsi;
+		size_t size = (size_t)f -> R.rdx;
+
+		validate_user_buffer(buffer, size);
+
+		if(fd == 1)
+		{
+			putbuf(buffer, size);
+			f -> R.rax = size;
+		}
+		else
+		{
+			f -> R.rax = -1; 
+		}
+		return; 
+	}
+
+	case SYS_CREATE: 
+	{
+		// syscall2에서 커널이 아래처럼 정보를 가져옴
+		const char *file = (const char * ) f -> R.rdi; 
+		unsigned initial_size = (unsigned) f -> R.rsi;
+
+		struct thread *curr = thread_current();
+		validate_user_addr(file); 
+
+		
+/* 이제 null검사는 is_user_vaddr에서 하니까 필요없음		
+		if(file == NULL) //create(NULL , 0); 
+		{
+			curr -> exit_code = -1; //현재 프로세스를 죽여
+			thread_exit();
+		//유효하지 않은 메모리 주소를 넘김 -> 프로그램 에러
+
+		}
+*/		
+
+		if(file[0] == '\0') //create("", 0); 
+		{
+			f -> R.rax = false;
+			return;
+		//정상적인 주소를 줬지만, 내용이 비어있어 파일 생성 실패(false) 반환
+		
+		}
+		
+		//유저가 넘긴 주소 검증한다. 
+		if(!is_user_vaddr(file))
+		{
+			curr -> exit_code = -1;
+			thread_exit();
+		}
+		
+		//lock 사용: 
+		lock_acquire(&filesys_lock);
+		bool ok = filesys_create(file, initial_size);
+		lock_release(&filesys_lock);
+		
+		f-> R.rax = ok;
+		return; 
+
+	/* NICK */
+	}
 
 	case SYS_EXIT:
 	/* KDA'S CODE - start */
